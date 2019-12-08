f9e9165d5ee063a58f7f4532d0dc831590d65a7d
Jake Gealer <jake@gealer.email>
2019-12-08 16:50:20 +0000

    handle cors

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ async function handleRequest(request) {
   } else if (u.pathname === "/add") {
     const url = u.searchParams.get("url")
     if (!url) {
-      return new Response("Link not found.", { status: 400 })
+      return new Response("Link not found.", { status: 400, headers: {"Access-Control-Allow-Origin": "*"}, })
     }
     const id = createId()
     await LINKS.put(id, url)
-    return new Response(`https://s.magiccap.me/${id}`, { status: 200 })
+    return new Response(`https://s.magiccap.me/${id}`, { status: 200, headers: {"Access-Control-Allow-Origin": "*"}, })
   }
   const url = await LINKS.get(u.pathname.substr(1))
   if (url) {
     return Response.redirect(url)
   } else {
-    return new Response("Link not found.", { status: 404 })
+    return new Response("Link not found.", { status: 404, headers: {"Access-Control-Allow-Origin": "*"}, })
   }
 }
